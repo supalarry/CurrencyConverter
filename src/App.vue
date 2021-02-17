fca24bff7c041bc6aa44bb5e0e772fc5f4f7b0c2
Larry <lauris.skraucis@gmail.com>
2021-02-17 09:58:42 +0100

    universal styling for all elements

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
 </template>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
